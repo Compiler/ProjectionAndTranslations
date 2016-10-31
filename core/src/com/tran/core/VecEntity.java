2cf67398e957e0c8740683c85b9c42c59a2e4b67
Luke <lr98lr@aol.com>
2016-10-31 03:44:13 -0400

    Cleaned

--- core/src/com/tran/core/VecEntity.java
+++ core/src/com/tran/core/VecEntity.java
@@ -7,78 +7,57 @@ import com.badlogic.gdx.math.Matrix3;
 import com.badlogic.gdx.math.Vector2;
 
 public class VecEntity {
-	
-	
-	
-	private Vector2 pos;
-	private float[][] mat2 = {{ -1.0f,  0.0f		},
-							 {	 0.0f, -2.0f	}};
-	
-	private float[][] mat23 = {{ -1.0f,  0.0f		},
-			 				{	 0.0f, -2.0f	}};
 
-	public VecEntity(float x, float y){
-		
-		pos = new Vector2(x,y);
-		
+	private Vector2 pos;
+	private float[][] mat2 = { { -1.0f, 0.0f }, { 0.0f, -2.0f } };
+
+	public VecEntity(float x, float y) {
+
+		pos = new Vector2(x, y);
+
 	}
-	
-	
-	
-	
-	public void trans(){
-		
-		
-	}
-	float elapsedTime = 0;
+
 	boolean go;
 	float newX = 0;
 	float newY = 0;
 	float speed = 0;
-	public void render(ShapeRenderer render){
-		if(Gdx.input.isKeyJustPressed(Keys.SPACE)){
+
+	public void render(ShapeRenderer render) {
+		if(Gdx.input.isKeyJustPressed(Keys.SPACE)) {
 			go = true;
 			newX = mat2[0][0] * pos.x + mat2[1][0] * pos.y;
 			newY = mat2[0][1] * pos.x + mat2[1][1] * pos.y;
 		}
-		
-		
-		
-		
-		if(go){
+
+		if(go) {
 			speed += 10 * Gdx.graphics.getDeltaTime();
-			elapsedTime += Gdx.graphics.getDeltaTime();
-			if(pos.x >= newX + 5 || pos.x <= newX - 5){
-				if(pos.x < newX){
+			if(pos.x >= newX + 5 || pos.x <= newX - 5) {
+				if(pos.x < newX) {
 					pos.x += speed;
-				}else
+				} else
 					pos.x -= speed;
-			}else{
-				
+			} else {
+
 			}
-			if(pos.y >= newY + 5 || pos.y <= newY - 5){
-				if(pos.y < newY){
+			if(pos.y >= newY + 5 || pos.y <= newY - 5) {
+				if(pos.y < newY) {
 					pos.y += speed;
-				}else
+				} else
 					pos.y -= speed;
-			}else{
-				
+			} else {
+
 			}
 		}
-		
-		
-		
-		
+
 		render.setColor(0.0f, 1.0f, 0.0f, 1.0f);
 		render.line(Vector2.Zero.x, Vector2.Zero.y, pos.x, 0.0f);
-		
+
 		render.setColor(1.0f, 0.0f, 0.0f, 1.0f);
 		render.line(pos.x, Vector2.Zero.y, pos.x, pos.y);
-		
+
 		render.setColor(0.0f, 1.0f, 1.0f, 1.0f);
 		render.line(Vector2.Zero, pos);
-		
-		
+
 	}
 
 }
